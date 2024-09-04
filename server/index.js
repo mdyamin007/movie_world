485198fc670979b7f5c57d6fff60b8aecf550b9d
Md. Yamin <mohammed.yamin@dsinnovators.com>
2024-09-04 11:44:51 +0600

    mongodb connected

--- server/index.js
+++ server/index.js
@@ -1,13 +1,24 @@
 import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+import mongoose from "mongoose"
+import "dotenv/config";
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    res.send('Hello world!')
-})
-
-app.listen(port, () => {
-    console.log(`Listeing on port ${port}`);
+mongoose.connect(process.env.MONGODB_URL).then(() => {
+    console.log("MongoDB Connected...");
+    app.listen(port, () => {
+        console.log(`Listeing on port ${port}`);
+    })
+}).catch(err => {
+    console.log(err);
+    process.exit(1);
 })
